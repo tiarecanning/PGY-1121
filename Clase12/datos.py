c3dbf63fa12a795d7c79bd9b4e3f9e2913cfcf16
tiarecanning <134552818+tiarecanning@users.noreply.github.com>
2023-05-24 16:43:49 -0400

    Add files via upload

---
+++ Clase12/datos.py
@@ -0,0 +1,9 @@
+#Ejemplo de uso de github
+print("Ingreso de datos")
+print("***********************/n")
+vNom= input("Ingrese su nombre: ")
+vEdad= input("Ingrese su edad: ")
+print("***************************")
+print(f"Su nombre es: {vNom} ")
+print(f"Su edad es: {vEdad} ")
+print("Programa finalizado. ")
